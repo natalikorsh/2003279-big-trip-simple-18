6c553df419b8ade056b0c0401e0d547a51c3794b
Natalia <tonio1811@gmail.com>
2022-08-10 11:49:53 +0200

    Время разбивать камни

---
+++ src/presenter/page-presenter.js
@@ -0,0 +1,21 @@
+import CreateFormView from "../view/create-form-view";
+import EditFormView from "../view/edit-form-view";
+import RoutePointView from "../view/route-point-view";
+import SortView from "../view/sort-view";
+import { render } from "../render";
+
+export default class PagePresenter {
+  pageForm = new CreateFormView();
+
+  init = (pageContainer) => {
+    this.pageContainer = pageContainer;
+
+    render(new SortView(), this.pageContainer);
+    render(this.pageForm, this.pageContainer);
+    render(new EditFormView(), this.pageForm.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new RoutePointView, this.pageForm.getElement());
+    }
+  }
+}
